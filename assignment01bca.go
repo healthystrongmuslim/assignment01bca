7611edade662c14ddcfb696faf6d09b07bf8005e
Muhammad Musaab <146497620+healthystrongmuslim@users.noreply.github.com>
2026-03-05 20:32:28 +0500

    Use strings for hashes and compute block hash
    Change block struct fields prevHash, hash and merkleRoot from [32]byte to string and add an index field.
    Add byte32toStr and CalculateHash helpers and import strconv. Update NewBlock signature to accept index, set prevHash as a string, compute merkleRoot via CalculateHash, build a header (prevHash + nonce + merkleRoot + creationTime + index) and store the block hash as the SHA-256 of that header.

--- assignment01bca.go
+++ assignment01bca.go
@@ -2,42 +2,52 @@ package assignment01bca
 
 import (
 	"crypto/sha256"
+	"strconv"
 	"time"
 )
 
 type block struct {
 	prevBlock    *block
-	prevHash     [32]byte
-	hash         [32]byte // = sha256(prevHash, nonce, merkleRoot)
+	prevHash     string
+	hash         string // = sha256(prevHash, nonce, merkleRoot, creationTime, index)
 	nonce        int
+	index        int
 	creationTime time.Time
-	merkleRoot   [32]byte //the hash of the transaction data (naive i.e. O(n) way for simplicity for now)
+	merkleRoot   string //the hash of the transaction data (naive i.e. O(n) way for simplicity for now)
 	transaction  string
 }
 
-func NewBlock(transaction string, nonce int, prev *block) *block {
-	nblock := &block{prevBlock: prev, nonce: nonce, creationTime: time.Now(), transaction: transaction}
+func byte32toStr(data [32]byte) string {
+	return string(data[:])
+}
+
+func CalculateHash(stringToHash string) string {
+	return byte32toStr(sha256.Sum256([]byte(stringToHash)))
+}
+
+func NewBlock(transaction string, nonce int, prev *block, index int) *block {
+	nblock := &block{prevBlock: prev, nonce: nonce, creationTime: time.Now(), transaction: transaction, index: index}
 	if prev != nil {
 		nblock.prevHash = prev.hash
 	} else {
-		nblock.prevHash = [32]byte{0}
+		nblock.prevHash = ""
 	}
-	nblock.merkleRoot = sha256.Sum256([]byte(transaction))
+	nblock.merkleRoot = CalculateHash(transaction)
+	header := nblock.prevHash + strconv.Itoa(nonce) + nblock.merkleRoot + nblock.creationTime.String() + strconv.Itoa(index)
+	nblock.hash = CalculateHash(header)
 	return nblock
 }
 
 /*
-func ListBlocks() {
-	"A method to print all the blocks in a nice format showing block data such as transaction, nonce, previous hash, current block hash"
-}
+	func ListBlocks() {
+		"A method to print all the blocks in a nice format showing block data such as transaction, nonce, previous hash, current block hash"
+	}
 
-func ChangeBlock() {
-	"function to change block transaction of the given block ref"
-}
-func VerifyChain() {
-	"function to verify blockchain in case any changes are made."
-}
-func CalculateHash(stringToHash string) {
-	"function for calculating hash of a block. Hash must be calculated using SHA-256 and must concatenate: transaction + nonce + previousHash + index + timestamp."
-}
+	func ChangeBlock() {
+		"function to change block transaction of the given block ref"
+	}
+
+	func VerifyChain() {
+		"function to verify blockchain in case any changes are made."
+	}
 */
